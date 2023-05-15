80e0a90e032b825e26a612d64038b1b9b3326157
adel-R <adelremadi@gmail.com>
2023-05-15 19:48:40 +0200

    util streamlit

--- streamlit/util/modeling.py
+++ streamlit/util/modeling.py
@@ -41,24 +41,45 @@ def set_reproducible():
     torch.cuda.manual_seed(random_seed)
 
 
-def get_datasets(config):
+def init_data(config):
 
     # get transforms
     trn_transform, valtst_transform = prepData.get_transform() # to be applied during training
 
     # fetch metadata
     trnval_metadata = loadData.load_metadata(config["dir"] + "\\images\\metadata.csv", set = "train")
-    tst_metadata    = loadData.load_metadata(config["dir"] + "\\images\\metadata.csv", set = "test")
+    if config["official_test"]:
+        tst_metadata    = loadData.load_metadata(config["dir"] + "\\images\\metadata.csv", set = "test")
 
-    # perform train validation split
-    trn_metadata, val_metadata = loadData.trainval_split(trnval_metadata, val_size = 0.2)
+    # perform train val test split
+    if config["official_test"]:
+        folds, _ = loadData.stratified_split(trnval_metadata, k = config["k_cv"])
+    else:
+        folds, tst_metadata = loadData.stratified_split(trnval_metadata, k = config["k_cv"], test_size = config["artificial_test_size"])
 
     # get pytorch datasets for modeling
-    trn_dataset = loadData.CustomDataset(trn_metadata, transform = trn_transform, resize = config["resize"], apply_CLAHE = config["apply_CLAHE"], dir = config["dir"])
-    val_dataset = loadData.CustomDataset(val_metadata, transform = valtst_transform, resize = config["resize"], apply_CLAHE = config["apply_CLAHE"], dir = config["dir"])
-    tst_dataset = loadData.CustomDataset(tst_metadata, transform = valtst_transform, resize = config["resize"], apply_CLAHE = config["apply_CLAHE"], dir = config["dir"])
+    trn_datasets = []
+    val_datasets = []
+    for k in range(config["k_cv"]):
+        trn_dataset = loadData.CustomDataset(folds[k]["train"].reset_index(), transform = trn_transform, resize = config["resize"], apply_CLAHE = config["apply_CLAHE"], dir = config["dir"])
+        val_dataset = loadData.CustomDataset(folds[k]["val"].reset_index(),   transform = valtst_transform, resize = config["resize"], apply_CLAHE = config["apply_CLAHE"], dir = config["dir"])
+        trn_datasets.append(trn_dataset)
+        val_datasets.append(val_dataset)
 
-    return trn_dataset, val_dataset, tst_dataset
+    tst_dataset = loadData.CustomDataset(tst_metadata.reset_index(), transform = valtst_transform, resize = config["resize"], apply_CLAHE = config["apply_CLAHE"], dir = config["dir"])
+
+    # get pytorch dataloaders
+    trnloaders = []
+    valloaders = []
+    for trn_dataset, val_dataset in zip(trn_datasets, val_datasets):
+        trnloader = get_dataloader(trn_dataset, config["batch_size"], shuffle = True)
+        valloader = get_dataloader(val_dataset, config["batch_size"], shuffle = False)
+        trnloaders.append(trnloader)
+        valloaders.append(valloader)
+    
+    tstloader = get_dataloader(tst_dataset, config["batch_size"], shuffle = False)
+
+    return trnloaders, valloaders, tstloader
 
 
 def get_dataloader(dataset, batch_size, shuffle = True):
@@ -113,15 +134,6 @@ def init_training(config, use_model = None):
     # initialise device
     device, model = get_device(model)
 
-    # get data
-    trn_dataset, val_dataset, tst_dataset = get_datasets(config)
-    trnloader = get_dataloader(trn_dataset, config["batch_size"], shuffle = True)
-    valloader = get_dataloader(val_dataset, config["batch_size"], shuffle = False)
-    try:
-        tstloader = get_dataloader(tst_dataset, config["batch_size"], shuffle = False)
-    except ValueError: # if no test data available
-        tstloader = None
-
     # initialise optimizer
     optimizer = torch.optim.SGD(params = model.parameters(), lr = config["lr"], momentum = config["mom"], weight_decay = config["wd"])
 
@@ -144,7 +156,7 @@ def init_training(config, use_model = None):
         criterion = nn.BCEWithLogitsLoss()
         # target_type logits --> PyTorch softmaxes within its code and expects raw logits as input
         
-    return (model, optimizer, trnloader, valloader, tstloader, scheduler, scheduler_step, criterion, device)
+    return (model, optimizer, scheduler, scheduler_step, criterion, device)
 
 
 def train_epoch(dataloader, model, optimizer, scheduler, scheduler_step, criterion, device):
@@ -221,69 +233,90 @@ def test_epoch(dataloader, model, criterion, device):
     return acc, auc, loss_mean
 
 
-def train_evaluate_model(config, verbose = True, ray = False, return_obj = True, use_model = None):
+def get_metric_from_matrix(matrix, dict_idxs):
+    """
+    Returns mean of maxs of columns
+    """
+    result = []
+    for k, epoch in dict_idxs.items():
+        result.append(matrix[epoch, k])
+    return np.mean(result)
+
+
+def train_evaluate_model(config, trnloaders, valloaders, tstloader, verbose = True, ray = False, return_obj = True, use_model = None):
     """
     Function that aggregates everything in one place to start model training.
     """
     # maximum number of epochs to train
     max_epochs = int(config["max_epochs"])
-    
+
     # train and evaluate the model
-    trn_losses = np.zeros(max_epochs)
-    val_losses = np.zeros(max_epochs)
-    trn_accs = np.zeros(max_epochs)
-    val_accs = np.zeros(max_epochs)
-    
+    trn_losses = np.zeros((max_epochs, config["k_cv"]))
+    val_losses = np.zeros((max_epochs, config["k_cv"]))
+    trn_accs = np.zeros((max_epochs, config["k_cv"]))
+    val_accs = np.zeros((max_epochs, config["k_cv"]))
+    trn_aucs = np.zeros((max_epochs, config["k_cv"]))
+    val_aucs = np.zeros((max_epochs, config["k_cv"]))
+
+    best_models_indicies = {}
     print(f'We have started training') if verbose else None
-       
-    # set up model
-    (model, optimizer, trnloader, valloader, tstloader, scheduler, scheduler_step, criterion, device) = init_training(config)
 
-    # perform training
-    max_val_auc = 0
+    # iterate over all train and testloaders (to handle kfolds training)
+    for k, (trnloader, valloader) in enumerate(zip(trnloaders, valloaders)):
+        
+        print(f"Starting fold [{k+1}/{config['k_cv']}]") if verbose else None
+
+        # set up model
+        (model, optimizer, scheduler, scheduler_step, criterion, device) = init_training(config)
+
+        # perform training
+        max_val_auc = 0
+
+        for epoch in range(max_epochs):
+
+            ##TRAINING##
+            trn_acc, trn_auc, trn_loss = train_epoch(trnloader, model, optimizer, 
+                                                     scheduler, scheduler_step,
+                                                     criterion, device)
+
+            ##VALIDATION##
+            val_acc, val_auc, val_loss = test_epoch(valloader, model, criterion, device)
+
+            ##SCHEDULE learning rate if necessary##
+            if scheduler_step == "every epoch":
+                scheduler.step()
+
+            ##LOGGING##
+            trn_losses[epoch, k] = trn_loss
+            val_losses[epoch, k] = val_loss
+            trn_accs[epoch, k] = trn_acc
+            val_accs[epoch, k] = val_acc
+            trn_aucs[epoch, k] = trn_auc
+            val_aucs[epoch, k] = val_auc
+            
+            ##REPORT##
+            if verbose:
+                print(f"Fold [{k+1}/{config['k_cv']}], Epoch [{epoch + 1}/{max_epochs}] --> Trn Loss: {round(trn_loss, 2)}, \
+Val Loss: {round(val_loss, 2)}, Trn AUC: {round(trn_auc, 3)}, Val AUC: {round(val_auc, 3)}")
+            
+            if config["save"]:
+                if val_auc >= max_val_auc:
+                    max_val_auc = val_auc
+                    path = f"best_{config['model']}_fold{k}.pt"
+                    torch.save(model.state_dict(), path)
+                    best_models_indicies[k] = epoch # needed as we are reporting multiple metrics
     
-    for epoch in range(max_epochs):
-
-        ##TRAINING##
-        trn_acc, trn_auc, trn_loss = train_epoch(trnloader, model, optimizer,
-                                        scheduler, scheduler_step,
-                                        criterion, device)
-
-        ##VALIDATION##
-        val_acc, val_auc, val_loss = test_epoch(valloader, model, criterion, device)
-
-        ##SCHEDULE learning rate if necessary##
-        if scheduler_step == "every epoch":
-            scheduler.step()
-
-        ##LOGGING##
-        trn_losses[epoch] = trn_loss
-        val_losses[epoch] = val_loss
-        trn_accs[epoch] = trn_acc
-        val_accs[epoch] = val_acc
-            
-        ##REPORT##
-        if verbose:
-            print(f"Epoch [{epoch + 1}/{max_epochs}] -> Trn Loss: {round(trn_loss, 2)}, Val Loss: {round(val_loss, 2)}, \
-Trn AUC: {round(trn_auc, 3)}, Val AUC: {round(val_auc, 3)}")
-            
-        if config["save"]:
-            if val_auc >= max_val_auc:
-                max_val_auc = val_auc
-                path = f"best_{config['model']}.pt"
-                torch.save(model.state_dict(), path)
-
-        if ray:
-            session.report({"trn_loss": trn_loss, "val_loss": val_loss,
-                            "trn_acc": trn_acc, "val_acc": val_acc,
-                            "trn_auc": trn_auc, "val_auc": val_auc})
+    if ray:
+        session.report({"trn_loss": get_metric_from_matrix(trn_losses, best_models_indicies), "val_loss": get_metric_from_matrix(val_losses, best_models_indicies),
+                        "trn_acc": get_metric_from_matrix(trn_accs, best_models_indicies), "val_acc": get_metric_from_matrix(val_accs, best_models_indicies),
+                        "trn_auc": get_metric_from_matrix(trn_aucs, best_models_indicies), "val_auc": get_metric_from_matrix(val_aucs, best_models_indicies)})
     
     if len(tstloader) > 0:
         tst_acc, tst_auc, tst_loss = test_epoch(tstloader, model, criterion, device)
         print(f"Test Performance -> Loss: {round(tst_loss, 2)}, AUC: {round(tst_auc, 3)}")
 
     if return_obj:
-        return model, trn_losses, val_losses, trn_accs, val_accs
+        return model, trn_losses, val_losses, trn_accs, val_accs, trn_aucs, val_aucs
     
 
 def ray_trainable(config):
@@ -292,9 +325,12 @@ def ray_trainable(config):
     """
     # ensure reproducibility (for meaningful hyperparameter selection that does not depend on random seed behavior)
     set_reproducible()
+
+    # initialise objects for training
+    trnloaders, valloaders, tstloader = init_data(config)
     
     # perform training (no return!)
-    train_evaluate_model(config, verbose = config["verbose"], ray = True, return_obj = False)
+    train_evaluate_model(config, trnloaders, valloaders, tstloader, verbose = config["verbose"], ray = True, return_obj = False)
 
 
 def trial_str_creator(trial):
